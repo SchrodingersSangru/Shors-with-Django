45413b300662276fb6d0bcdd7355059102b67de6
SchrodingersSangru <deshpande.sangram156@gmail.com>
2022-08-12 13:08:11 +0530

    updated the api key functionality

--- QFactors/QFactors/settings.py
+++ QFactors/QFactors/settings.py
@@ -16,14 +16,23 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
+import environ
+
+env = environ.Env(
+    # set casting, default value
+    DEBUG=( bool , False)
+)
+
+environ.Env.read_env() 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
+
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-rkvz)$tx7piq1j56es*gl0uc7p7@p_p@lme*qng6q3^m#8!_2x'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = ['*']
 
